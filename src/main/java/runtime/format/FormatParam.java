4f8fe40e56708473fae4523ea9628831af63765c
ChrisItisdud <chris.reichel@gmx.net>
2022-05-02 23:15:27 +0200

    added license text to all source files

--- src/main/java/runtime/format/FormatParam.java
+++ src/main/java/runtime/format/FormatParam.java
@@ -1,3 +1,9 @@
+/*
+    COPYRIGHT 2022 Chris Samira Reichel
+    This software is published under the MIT license.
+    The license text can be found at https://opensource.org/licenses/MIT
+ */
+
 package runtime.format;
 
 public interface FormatParam{
